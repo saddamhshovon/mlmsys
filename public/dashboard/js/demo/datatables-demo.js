6a34ffb75ad3214d9bb7816d2405328adef53ff3
Shimul Chakraborty <shimul.ckbt@gmail.com>
2022-04-21 22:06:14 +0600

    datatable extra entry length

--- public/dashboard/js/demo/datatables-demo.js
+++ public/dashboard/js/demo/datatables-demo.js
@@ -1,4 +1,6 @@
 // Call the dataTables jQuery plugin
-$(document).ready(function() {
-  $('#dataTable').DataTable();
+$(document).ready(function () {
+  $('#dataTable').DataTable({
+    "lengthMenu": [[10, 25, 50, 100, 200, 500, 1000, -1], [10, 25, 50, 100, 200, 500, 1000, "All"]]
+  });
 });
